cbd04eace40dffc80f9f5b912d0ec90cef27350c
skulu <76804157+skulu@users.noreply.github.com>
2022-03-16 21:02:21 +0800

    format

--- 4_connectedComponentsCount.py
+++ 4_connectedComponentsCount.py
@@ -1,32 +1,31 @@
 def connected_components_count(graph):
     count = 0
     visited = set()
-    
+
     for node in graph:
-        if node not in visited: 
+        if node not in visited:
             dfs(graph, node, visited)
             count += 1
-    
+
     return count
 
 
-def dfs(graph, source, visited = None):
-    if visited is None: visited = set()
-    
-    if source in visited: return 0
+## Recursive solution ##
+def dfs(graph, source, visited=None):
+    if visited is None:
+        visited = set()
+
+    if source in visited:
+        return 0
     visited.add(source)
 
     for current in graph[source]:
-        if current not in visited: dfs(graph, current, visited)
+        if current not in visited:
+            dfs(graph, current, visited)
 
-graph = {
-    0: [8, 1, 5],
-    1: [0],
-    5: [0, 8],
-    8: [0, 5],
-    2: [3, 4],
-    3: [2, 4],
-    4: [3, 2]
-}
+
+## Iterative solution ##
+
+graph = {0: [8, 1, 5], 1: [0], 5: [0, 8], 8: [0, 5], 2: [3, 4], 3: [2, 4], 4: [3, 2]}
 
 print(connected_components_count(graph))
