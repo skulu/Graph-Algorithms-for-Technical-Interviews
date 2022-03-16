419399f785ec23ad2025214e66bed17280ac1658
skulu <76804157+skulu@users.noreply.github.com>
2022-03-16 22:42:41 +0800

    recursive largest component

---
+++ 5_largestComponent.py
@@ -0,0 +1,55 @@
+## https://structy.net/problems/largest-component
+
+
+def largest_component(graph):
+    visited = set()
+    longest = 0
+
+    for node in graph:
+        size = dfs(graph, node, visited)
+
+        if size > longest:
+            longest = size
+
+    return longest
+
+
+## Recursive solution ##
+
+
+def dfs(graph, source, visited=None):
+    if visited is None:
+        visited = set()
+
+    if source in visited:
+        return 0
+    visited.add(source)
+    size = 1
+
+    for current in graph[source]:
+        size += dfs(graph, current, visited)
+        
+    return size
+
+
+## Iterative solution ##
+
+
+# def dfs(graph, node, visited):
+#     if node in visited:
+#         return False
+#     stack = [node]
+
+#     while len(stack) > 0:
+#         current = stack.pop()
+#         if current in visited:
+#             continue
+#         visited.add(current)
+#         for neighbour in graph[current]:
+#             stack.append(neighbour)
+
+#     return True
+
+graph = {0: [8, 1, 5], 1: [0], 5: [0, 8], 8: [0, 5], 2: [3, 4], 3: [2, 4], 4: [3, 2]}
+
+print(largest_component(graph))
